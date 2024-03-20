b17db86c90e6f6382939a7d7f6c9d8f87a499686
Xola Bangaza <xolabangaza@gmail.com>
2024-03-20 15:36:17 +0200

    exercise

--- doubleNumbers.js
+++ doubleNumbers.js
@@ -9,10 +9,28 @@ for (let i = 0; i < Arr.length; i++) {
 let Arr2 = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 let copyArr = []
 
-function double  (Arr , n) {
+function double  (Arr , n ,) {
     for( i=0; i <Arr.length ; i++ ) {
         copyArr[i] = Arr[i] *n
     }
     return copyArr
 }
-console.log(double(Arr2,2))
+// console.log(double(Arr2,2))
+
+//  let age = 5;
+function fnonvoid( age) {
+
+    const result = age+1;
+    return result
+}
+console.log(fnonvoid(5))
+
+function after10y(newAge) {
+    return(Age + 10);
+}
+console.log(after10y (fnonvoid)(5));
+function fvoid( age) {
+
+    const result = age+1;
+}
+console.log(fvoid(5))
---
+++ function-Ex.js
@@ -0,0 +1,21 @@
+// let reversed ="sinaye"
+
+// function reverseString(str) {
+//     return str.split('').reverse().join('');
+// }
+
+// let result = reverseString(reversed);
+// console.log(result); 
+// let original = "sinaye";
+// let result = reverseString("Sinaye");
+
+let reversed = "";
+function reverseString(str) {
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
+}
+
+console.log(reverseString("DJ")); 
+
